147324dcec5944620c87c066c8d2ea9268ebadf2
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-03-22 07:00:00 +0100

    + A first functional spec for whole tree transformation

---
+++ lib/tree_transform.rb
@@ -0,0 +1,11 @@
+
+
+# Transforms an expression tree into something else. You decide what. 
+#
+class TreeTransform
+  def rule(expression, &block)
+  end
+  
+  def apply(obj)
+  end
+end
---
+++ spec/tree_transform_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+require 'tree_transform'
+
+describe TreeTransform do
+  attr_reader :transform
+  before(:each) do
+    @transform = TreeTransform.new
+  end
+  
+  class A < Struct.new(:elt); end
+  class B < Struct.new(:elt); end
+  class C < Struct.new(:elt); end
+  
+  describe "given :_x => A.new(x)" do
+    before(:each) do
+      transform.rule(:_x) { |d| A.new(d[:x]) }
+    end
+    it "should transform 'a' into A.new('a')" do
+      transform.apply('a').should == A.new('a')
+    end 
+  end
+  describe "given rules on {:a => :_x} and {:b => :_x}" do
+    it "should transform {:a=>{:b=>'c'}} into A(B('c'))" do
+      transform.apply({:a=>{:b=>'c'}}).should == A.new(B.new('c'))
+    end
+  end
+end
