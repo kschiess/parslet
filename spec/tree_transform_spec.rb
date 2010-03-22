299df7ad3b610fe53f55ea8c7ba56ebe471decec
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-03-22 18:58:33 +0100

    . Can't find any cases with hashes anymore that dont already work

--- spec/tree_transform_spec.rb
+++ spec/tree_transform_spec.rb
@@ -11,6 +11,7 @@ describe TreeTransform do
   class A < Struct.new(:elt); end
   class B < Struct.new(:elt); end
   class C < Struct.new(:elt); end
+  class Bi < Struct.new(:a, :b); end
   
   describe "given :_x => A.new(x)" do
     before(:each) do
@@ -26,8 +27,23 @@ describe TreeTransform do
       transform.rule(:b => :_x) { |d| B.new(d[:x]) }
     end
     
+    it "should transform {:d=>{:b=>'c'}} into d => B('c')" do
+      transform.apply({:d=>{:b=>'c'}}).should == {:d => B.new('c')}
+    end
     it "should transform {:a=>{:b=>'c'}} into A(B('c'))" do
       transform.apply({:a=>{:b=>'c'}}).should == A.new(B.new('c'))
     end
+  end
+  describe "pulling out subbranches" do
+    before(:each) do
+      transform.rule(:a => {:b => :_x}, :d => {:e => :_y}) { |d|
+        Bi.new(*d.values_at(:x, :y))
+      }
+    end
+    
+    it "should yield Bi.new('c', 'f')" do
+      transform.apply(:a => {:b => 'c'}, :d => {:e => 'f'}).should ==
+        Bi.new('c', 'f')
+    end 
   end
 end
