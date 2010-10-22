17ab5d4278ed05f4aa1a3679304a21639073edc9
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-10-22 07:27:27 +0200

    + spec for X.maybe

--- spec/unit/parslet/atom_results_spec.rb
+++ spec/unit/parslet/atom_results_spec.rb
@@ -8,8 +8,12 @@ describe 'Result of a Parslet#parse' do
     let(:parslet) { foo.maybe }
     
     context "when given no matching input" do
-      subject { parslet.parse('bar') }
+      subject { parslet.parse('') }
       it { should == nil }
+    end
+    context "when matching" do
+      subject { parslet.parse('foo') }
+      it { should == 'foo' }
     end
   end
   
