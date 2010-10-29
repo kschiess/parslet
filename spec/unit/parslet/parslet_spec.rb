80e9812efc0a259e9413ea8a026dc3070a0af5c0
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-10-29 14:53:33 +0200

    + root definition for parser classes - helper fun

--- spec/unit/parslet/parslet_spec.rb
+++ spec/unit/parslet/parslet_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 describe Parslet do
   include Parslet
   
+  describe "<- .root" do
+    let(:root_parslet) { flexmock() }
+    root :root_parslet
+    
+    it "should have defined a 'parse' method in the current context" do
+      root_parslet.should_receive(:parse).with('snaf').once
+      parse('snaf')
+    end 
+    it "should have defined a 'root' method, returning the root" do
+      root.should == root_parslet
+    end 
+  end
   describe "<- .rule" do
     # Rules define methods. This can be easily tested by defining them right 
     # here. 
