a983dc321f3c06df68a6818abbe7c3dea6664c4a
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-11-16 08:05:59 +0100

    . comment for parser

--- lib/parslet/parser.rb
+++ lib/parslet/parser.rb
@@ -1,6 +1,16 @@
 
-# A simple wrapper for the functionality to permit clean source code structure.
-# You should use this as the base class for your parsers. 
+# The base class for all your parsers. Use as follows: 
+#
+#   require 'parslet'
+#        
+#   class MyParser < Parslet::Parser
+#     rule(:a) { str('a').repeat }
+#     root(:a)        
+#   end
+#        
+#   pp MyParser.new.parse('aaaa')   # => 'aaaa'
+#   pp MyParser.new.parse('bbbb')   # => Parslet::Atoms::ParseFailed: 
+#                                   #    Don't know what to do with bbbb at line 1 char 1.
 #
 class Parslet::Parser 
   include Parslet
