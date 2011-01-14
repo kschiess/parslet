b9a12d18fd93b64eb27b0993a4a43a016ada23a6
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-01-14 07:15:33 +0100

    + documents the baseline

--- benchmarks/001.rb
+++ benchmarks/001.rb
@@ -5,6 +5,9 @@ include Parslet
 # Do a simple kind of benchmark using the built in Treetop parser. This 
 # gives the parser code a good workout without being too complex to write. 
 
+# time ruby benchmark/001.rb on my MacBook Air, user time
+# 14Jan2010       0m0.960s
+
 100.times do
   exp(%Q(
     ('a'* / 'b'* / 'c'*)?
