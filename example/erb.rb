ad813dca4c1a3b9f8ba59ad6b8e5745ef20ca814
Florian R. Hanke <florian.hanke@gmail.com>
2011-01-21 02:32:09 -0800

    A slightly bigger erb parser, including expressions.

--- example/erb.rb
+++ example/erb.rb
@@ -1,14 +1,14 @@
-
 $:.unshift File.dirname(__FILE__) + "/../lib"
 require 'parslet'
 
 class ErbParser < Parslet::Parser
-  rule(:ruby) { str('<%') >> (str('%>').absnt? >> any).repeat >> str('%>') }
-  rule(:html) { (str('<%').absnt? >> any).repeat(1) }
-  rule(:html_with_ruby) { (html.as(:html) | ruby.as(:ruby)).repeat }
-  root(:html_with_ruby)
+  rule(:ruby) { (str('%>').absnt? >> any).repeat.as(:ruby_code) }
+  rule(:possible_ruby_expression) { ((str('=') >> ruby).as(:ruby_expression) | ruby) }
+  rule(:erb_ruby) { str('<%') >> possible_ruby_expression >> str('%>') }
+  rule(:text) { (str('<%').absnt? >> any).repeat(1) }
+  rule(:text_with_ruby) { (text.as(:text) | erb_ruby).repeat }
+  root(:text_with_ruby)
 end
 
-p ErbParser.new.parse <<ERB
-The value of x is: <%= x %>
-ERB
+p ErbParser.new.parse "The value of x is <%= x %>."
+p ErbParser.new.parse "<% 1 + 2 %>"
