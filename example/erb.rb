ba896df05a632eea359932e9e8c5f6a77f8caa28
Florian R. Hanke <florian.hanke@gmail.com>
2011-01-21 02:50:04 -0800

    Added comment erb tag.

--- example/erb.rb
+++ example/erb.rb
@@ -2,13 +2,19 @@ $:.unshift File.dirname(__FILE__) + "/../lib"
 require 'parslet'
 
 class ErbParser < Parslet::Parser
-  rule(:ruby) { (str('%>').absnt? >> any).repeat.as(:ruby_code) }
-  rule(:possible_ruby_expression) { ((str('=') >> ruby).as(:ruby_expression) | ruby) }
-  rule(:erb_ruby) { str('<%') >> possible_ruby_expression >> str('%>') }
+  rule(:code) { (str('%>').absnt? >> any).repeat.as(:code) }
+  
+  rule(:expression) { (str('=') >> code).as(:expression) }
+  rule(:comment) { (str('#') >> code).as(:comment) }
+  rule(:erb) { expression | comment | code }
+  
+  rule(:erb_with_tags) { str('<%') >> erb >> str('%>') }
   rule(:text) { (str('<%').absnt? >> any).repeat(1) }
-  rule(:text_with_ruby) { (text.as(:text) | erb_ruby).repeat }
+  
+  rule(:text_with_ruby) { (text.as(:text) | erb_with_tags).repeat }
   root(:text_with_ruby)
 end
 
 p ErbParser.new.parse "The value of x is <%= x %>."
 p ErbParser.new.parse "<% 1 + 2 %>"
+p ErbParser.new.parse "<%# commented %>"
