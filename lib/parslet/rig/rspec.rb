dc85c294ce8ece9ec09958d7595de4a62427494c
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-02-10 08:03:40 +0100

    + Traces in error messages (on specifying :trace => true)

--- lib/parslet/rig/rspec.rb
+++ lib/parslet/rig/rspec.rb
@@ -1,4 +1,4 @@
-RSpec::Matchers.define(:parse) do |input|
+RSpec::Matchers.define(:parse) do |input, opts={}|
   chain(:as) { |as| @as = as }
 
   match do |parser|
@@ -6,19 +6,24 @@ RSpec::Matchers.define(:parse) do |input|
       @result = parser.parse(input)
       @as == @result or @as.nil?
     rescue Parslet::ParseFailed
+      @trace = parser.error_tree.ascii_tree if opts[:trace]
       false
     end
   end
 
   failure_message_for_should do |is|
-    "expected " << (@result ?
-      "output of parsing #{input.inspect} with #{is.inspect} to equal #{@as.inspect}, but was #{@result.inspect}" :
-      "#{is.inspect} to be able to parse #{input.inspect}")
+    "expected " << 
+      (@as ?
+        "output of parsing #{input.inspect} with #{is.inspect} to equal #{@as.inspect}, but was #{@result.inspect}" :
+        "#{is.inspect} to be able to parse #{input.inspect}") << 
+      (@trace ? 
+        "\n"+@trace : '')
   end
 
   failure_message_for_should_not do |is|
-    "expected " << (@as ?
-      "output of parsing #{input.inspect} with #{is.inspect} not to equal #{@as.inspect}" :
-      "#{is.inspect} to not parse #{input.inspect}, but it did")
+    "expected " << 
+      (@as ?
+        "output of parsing #{input.inspect} with #{is.inspect} not to equal #{@as.inspect}" :
+        "#{is.inspect} to not parse #{input.inspect}, but it did")
   end
 end
--- spec/parslet/rig/rspec_spec.rb
+++ spec/parslet/rig/rspec_spec.rb
@@ -14,6 +14,11 @@ describe 'rspec integration' do
   it { str('foo').as(:bar).should parse('foo').as({:bar => 'foo'}) }
   it { str('foo').as(:bar).should_not parse('foo').as({:b => 'f'}) }
 
+  # Uncomment to test error messages manually: 
+  # it { str('foo').should parse('foo', :trace => true).as('bar') }
+  # it { str('foo').should parse('food', :trace => true) }
+  # it { str('foo').should_not parse('foo', :trace => true).as('foo') }
+  # it { str('foo').should_not parse('foo', :trace => true) }
   # it { str('foo').should parse('foo').as('bar') }
   # it { str('foo').should parse('food') }
   # it { str('foo').should_not parse('foo').as('foo') }
