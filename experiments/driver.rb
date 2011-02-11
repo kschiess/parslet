8d372041cc9b45068b769d0e4df5b0a3333af34d
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-02-11 12:16:00 +0100

    .

--- experiments/driver.rb
+++ experiments/driver.rb
@@ -9,10 +9,11 @@ def num(input)
 end
 
 input_files = Dir['input/*chars.st'].sort_by { |file| num(file).to_i }
+libs = ARGV.empty? ? %w(parslet treetop) : ARGV
 
 Benchmark.bm do |bm|
   input_files.each do |input_name|
-    %w(parslet treetop).each do |lib|
+    libs.each do |lib|
       run_name = sprintf("%s-%06d", lib[0,4], num(input_name).to_i)
       bm.report(run_name) { run(lib, input_name) }
     end
