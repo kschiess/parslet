de4319205a9ae80b2ca2aaaec71e43376bf3d4f2
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-02-16 07:44:54 +0100

    + parse_with_debug now works on all parslets

--- spec/parslet/convenience_spec.rb
+++ spec/parslet/convenience_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'parslet/convenience' do
   require 'parslet/convenience'
+  include Parslet
   
   class FooParser < Parslet::Parser
     rule(:foo) { str('foo') }
@@ -9,40 +10,34 @@ describe 'parslet/convenience' do
   end
   
   describe 'parse_with_debug' do
-    before(:each) do
-      @parser = flexmock FooParser.new
-    end
+    let(:parser) { flexmock FooParser.new }
     context 'internal' do
       before(:each) do
         # Suppress output.
         #
-        @parser.should_receive(:puts).zero_or_more_times
+        parser.should_receive(:puts)
       end
       it 'should exist' do
-        lambda { @parser.parse_with_debug('anything') }.should_not raise_error(NoMethodError)
+        lambda { parser.parse_with_debug('anything') }.should_not raise_error(NoMethodError)
       end
       it 'should catch ParseFailed exceptions' do
-        lambda { @parser.parse_with_debug('bar') }.should_not raise_error(Parslet::ParseFailed)
+        lambda { parser.parse_with_debug('bar') }.should_not raise_error(Parslet::ParseFailed)
       end
       it 'should parse correct input like #parse' do
-        lambda { @parser.parse_with_debug('foo') }.should_not raise_error
+        lambda { parser.parse_with_debug('foo') }.should_not raise_error
       end
     end
     context 'output' do
       it 'should puts once for the error, and once for the tree' do
-        @parser.should_receive(:puts).twice
+        parser.should_receive(:puts).twice
         
-        @parser.parse_with_debug('incorrect')
+        parser.parse_with_debug('incorrect')
       end
-      # TODO Too specific?
-      #
-      # it 'should output the error and the error tree' do
-      #   @parser.should_receive(:puts).once.with('Expected "foo", but got "inc" at line 1 char 1')
-      #   @parser.should_receive(:puts).once.with('`- Expected "foo", but got "inc" at line 1 char 1')
-      #   
-      #   @parser.parse_with_debug('incorrect')
-      # end
     end
     
+    it "should work for all parslets" do
+      str('foo').parse_with_debug('foo')
+      match['bar'].parse_with_debug('a')
+    end 
   end
 end
