d2c5b4ff5f8b87450fb4223d4119a68a13c2e8c0
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-04-17 08:52:40 +0200

    - removes superfluous spec

--- spec/parslet/atoms/sequence_spec.rb
+++ spec/parslet/atoms/sequence_spec.rb
@@ -5,13 +5,6 @@ describe Parslet::Atoms::Sequence do
   
   let(:sequence) { described_class.new }
   
-  describe "<- #error_tree" do
-    context "when no error has been produced" do
-      subject { sequence.error_tree }  
-      
-      its(:children) { should be_empty }
-    end
-  end
   describe '>> shortcut' do
     let(:sequence) { str('a') >> str('b') }
     
