0c65250a6da62046eaabad3d4768b3eb0daac2af
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-05-24 16:30:24 +0200

    . documentation

--- lib/parslet/atoms/named.rb
+++ lib/parslet/atoms/named.rb
@@ -7,13 +7,13 @@
 #
 class Parslet::Atoms::Named < Parslet::Atoms::Base
   attr_reader :parslet, :name
-  def initialize(parslet, name) # :nodoc:
+  def initialize(parslet, name)
     super()
 
     @parslet, @name = parslet, name
   end
   
-  def apply(source, context) # :nodoc:
+  def apply(source, context)
     success, value = result = parslet.apply(source, context)
 
     return result unless success
@@ -22,15 +22,15 @@ class Parslet::Atoms::Named < Parslet::Atoms::Base
         value))
   end
   
-  def to_s_inner(prec) # :nodoc:
+  def to_s_inner(prec)
     "#{name}:#{parslet.to_s(prec)}"
   end
 
-  def error_tree # :nodoc:
+  def error_tree
     parslet.error_tree
   end
 private
-  def produce_return_value(val) # :nodoc:
+  def produce_return_value(val)
     { name => flatten(val, true) }
   end
 end
