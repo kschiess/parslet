2a1d307c35c0aa8813348392a29b7fca9b06f081
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-08-01 20:13:54 -0700

    Whitespace cleanup

--- spec/parslet/convenience_spec.rb
+++ spec/parslet/convenience_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe 'parslet/convenience' do
   require 'parslet/convenience'
   include Parslet
-  
+
   class FooParser < Parslet::Parser
     rule(:foo) { str('foo') }
     root(:foo)
   end
-  
+
   describe 'parse_with_debug' do
     let(:parser) { flexmock FooParser.new }
     context 'internal' do
@@ -30,19 +30,19 @@ describe 'parslet/convenience' do
     context 'output' do
       it 'should puts once for tree output' do
         parser.should_receive(:puts).once
-        
+
         parser.parse_with_debug('incorrect')
       end
       it "should puts once for the error on unconsumed input" do
         parser.should_receive(:puts).once
 
         parser.parse_with_debug('foobar')
-      end 
+      end
     end
-    
+
     it "should work for all parslets" do
       str('foo').parse_with_debug('foo')
       match['bar'].parse_with_debug('a')
-    end 
+    end
   end
 end
