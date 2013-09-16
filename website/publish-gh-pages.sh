313b944bde6780594d080d5e54b1cdc0503b15a1
Kaspar Schiess <kaspar.schiess@absurd.li>
2013-09-16 10:36:52 +0200

    .

--- website/publish-gh-pages.sh
+++ website/publish-gh-pages.sh
@@ -2,4 +2,4 @@
 doc_sha=$(git ls-tree -d HEAD build | awk '{print $3}')
 git cat-file -p $doc_sha
 new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p gh-pages)
-git update-ref gh-pages $new_commit
+git update-ref refs/heads/gh-pages $new_commit
